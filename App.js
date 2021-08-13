570628d77daf6f802a5fd8793dbcaa2346ca6df9
PavelZ <pavelzubarevich@gmail.com>
2021-08-13 21:56:59 +0300

    start point

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
